73a048eafd91ddc53219ef5a85b6d778972b7b49
eaa <eaa4robo@gmail.com>
2023-06-19 13:33:46 +0300

    max map tested

--- src/System.cc
+++ src/System.cc
@@ -176,16 +176,18 @@ System::System(const string &strVocFile, const string &strSettingsFile, const eS
         //LOAD LOADED MAP!!!
         if(mbActivateLocalizationMode){
             vector<Map*> map_vector = mpAtlas->GetAllMaps();
-            std::cout<<"Number of Maps: "<< map_vector.size()<<std::endl;
             int biggest = 0;
             for(int i = 0; i < map_vector.size(); i++){
                 std::cout<< i <<" : "<< map_vector.at(i)->mnMaxKFid <<std::endl;
                 if(map_vector.at(i)->mnMaxKFid > map_vector.at(biggest)->mnMaxKFid)
                     biggest = i;
             }
-            mpAtlas->ChangeMap(map_vector.at(biggest));
+            mpAtlas->ChangeMap(map_vector.at(0));
         }else
-          mpAtlas->CreateNewMap();
+        {
+            mpAtlas->CreateNewMap();
+        }
+          
 
         //clock_t timeElapsed = clock() - start;
         //unsigned msElapsed = timeElapsed / (CLOCKS_PER_SEC / 1000);
